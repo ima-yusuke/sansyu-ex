bb7a68bdb30a0b273d05965aaf9d8fbee03d152d
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-05-09 13:59:24 +0900

    add:all file added3

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{$title}}</title>
+    @vite('resources/css/app.css')
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body class="h-full">
+{{$slot}}
+</body>
+
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors:{
+                "header_btn":"rgb(255,0,0)",
+                "main_blue":"#0832C9",
+                "main_bg":"#BEFFFA",
+                "interview_orange":"#FFDCB4",
+                "interview_blue":"#1BC6FC",
+                "interview_gray":"#626262",
+                "dpt_bg":"#FE9820",
+
+            },
+            spacing: {
+                "main_x":"460px",
+            },
+            width:{
+                "employee_width":"300px",
+                "450":"450px"
+            },
+            height:{
+                "employee_height":"300px",
+                "modalYoutube":"360px",
+            }
+        },
+    },
+
+    plugins: [forms],
+
+};
